987450a7fa902d89cebede6b416204aefcbc502d
Jo <34137982+andreas-barthel@users.noreply.github.com>
2021-07-05 16:24:55 +0200

    Update network.js
    First Unification specific settings

--- network.js
+++ network.js
@@ -1,33 +1,32 @@
 export default {
   id: 'cosmos-hub-testnet', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
-  name: 'Cosmos Stargate',
-  description:
-    'Cosmos is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
+  name: 'Unification',
+  description: 'Unification Blockchain',
   logo: `logo.svg`,
-  website: 'https://cosmos.network',
+  website: 'https://unification.com',
   apiURL: 'http://localhost:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
   rpcURL: 'ws://34.123.30.100:26657',
-  stakingDenom: 'MUON',
+  stakingDenom: 'FUND',
   coinLookup: [
     {
-      viewDenom: 'MUON',
-      chainDenom: 'umuon',
-      chainToViewConversionFactor: 1e-6,
+      viewDenom: 'FUND',
+      chainDenom: 'nund',
+      chainToViewConversionFactor: 1e-9,
       icon: `currencies/muon.png`,
     },
   ],
-  addressPrefix: 'cosmos',
-  validatorAddressPrefix: 'cosmosvaloper',
-  validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
-  HDPath: `m/44'/118'/0'/0/0`,
-  lockUpPeriod: `3 days`,
+  addressPrefix: 'und',
+  validatorAddressPrefix: 'undvaloper',
+  validatorConsensusaddressPrefix: 'undvalcons', // needed to map validators from staking queries to the validator set
+  HDPath: `m/44'/5555'/0'/0/0`,
+  lockUpPeriod: `21 days`,
   fees: {
     default: {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'MUON',
-          amount: 0.001,
+          denom: 'FUND',
+          amount: 0.002,
         },
       ],
     },
